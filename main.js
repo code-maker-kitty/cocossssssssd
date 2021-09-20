219111e5ee95211509d173409e63aab2ef77d87e
code-maker-kitty <77761014+code-maker-kitty@users.noreply.github.com>
2021-09-20 16:37:45 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,25 +2,32 @@ img = "";
 status = "";
 objects = [];
 function preload(){
-    img = loadImage('toy.jpg');
+
 }
 function setup(){
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(380, 380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 function draw() {
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
 if(status != ""){
+    r = random(255);
+    g = random(255);
+    b = random(255);
+    objectDetector.detect(video, gotResults);
     for (i = 0; i < objects.length; i++){
         document.getElementById("status").innerHTML = "Status : Object Detected";
-
-        fill("cyan");
+        document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+        fil(r,g,b);
         percent = floor(objects[i].confidence * 100);
         text(objects[i].label +" " + percent + "%", objects[i].x, objects[i].y);
         noFill();
-        stroke("cyan");
+        stroke(r,g,b);
         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
     }
 }
@@ -28,7 +35,6 @@ if(status != ""){
 function modelLoaded() {
     console.log("Model Loaded!")
     status = true;
-    objectDetector.detect(img, gotResults);
 }
 function gotResults(error, results){
     if (error) {
